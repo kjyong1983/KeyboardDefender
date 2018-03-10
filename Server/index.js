9051db96189464fb0194de58ffd86e9d97d00294
Dongwoon Son <dplusic@gmail.com>
2018-03-10 18:52:53 +0900

    Remove meaningless code

--- Server/index.js
+++ Server/index.js
@@ -2,10 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
     socket.on('send', function(data){
         io.emit('receive', data);
